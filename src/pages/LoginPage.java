290b3926a1a2fdc6bee6ad6bc11256f9875787bf
supriya1090 <supriyaashrit302@gmail.com>
2025-12-09 13:48:35 +0530

    First Commit

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class LoginPage {
+
+	public static void main(String args[]) {
+		System.out.println("Hello login page");
+	}
+}
---
+++ src/test/LoginPageTest.java
@@ -0,0 +1,8 @@
+package test;
+
+public class LoginPageTest {
+
+	public void test1() {
+		System.out.println("this is my test 1 -LoginPage");
+	}
+}
