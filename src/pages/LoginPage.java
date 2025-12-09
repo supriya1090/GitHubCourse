8ea12514c9514c3492eac778e081ae00ec5f25dd
supriya1090 <supriyaashrit302@gmail.com>
2025-12-09 14:09:10 +0530

    added/updated and deleted the files

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,5 +4,8 @@ public class LoginPage {
 
 	public static void main(String args[]) {
 		System.out.println("Hello login page");
+		int a = 10;
+		int b =5;
+		System.out.println("Sum of a and b is "+a+b);
 	}
 }
---
+++ src/pages/SearchPage.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class SearchPage {
+
+	public void search() {
+		System.out.println("Search page");
+	}
+}
