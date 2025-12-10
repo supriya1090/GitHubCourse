ef412e51ed62d36b0da63380c98cb00cd37649a7
supriya1090 <supriyaashrit302@gmail.com>
2025-12-10 22:21:07 +0530

    new files added

---
+++ src/pages/RegPage.java
@@ -0,0 +1,7 @@
+package pages;
+
+public class RegPage {
+
+	int i=1;
+	
+}
---
+++ src/test/RegPageTest.java
@@ -0,0 +1,9 @@
+package test;
+
+public class RegPageTest {
+
+	public void test2() {
+		System.out.println("this is my test 2 -RegPage");
+	}
+
+}
