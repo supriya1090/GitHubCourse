9e1cb5b8ccb2f2169e9336526280d26cd4908a57
supriya1090 <supriyaashrit302@gmail.com>
2025-12-23 16:48:55 +0530

    updated

--- src/test/RegPageTest.java
+++ src/test/RegPageTest.java
@@ -5,6 +5,14 @@ public class RegPageTest {
 	public void test2() {
 		System.out.println("this is my test 2 -RegPage");
 		System.out.println("this is my test 2 -RegPage");
+		String name="supriya";
+		int age =35;
+		String city ="Bengaluru";
 	}
+	/*
+	 * 4b38480 (HEAD -> master) update the regPage test ef412e5 new files added
+	 * af7909e (origin/master) modified pages class 8ea1251 added/updated and
+	 * deleted the files 290b392 First Commit
+	 */
 
 }
